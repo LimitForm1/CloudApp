de57dd6b41ae767e7d093b4aab153e6fb263fd8b
MateoZanette <126169000+LimitForm1@users.noreply.github.com>
2025-03-25 20:47:13 -0300

    Delete Back directory

--- Back/src/main/java/API/Back/Repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-//package API.Back.Repository;
-
-//import org.springframework.data.repository.CrudRepository;
-//import API.Back.Entity.Pessoa;
-
-//public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
-//}
